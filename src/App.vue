aae016e0858562df538fd71d2f26ba02d186ee0e
mecies <maciekhnat@gmail.com>
2020-04-29 19:32:19 +0200

    Project init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    siemano fresh data visualisation
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-};
+export default {};
 </script>
 
 <style lang="scss">
